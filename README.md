0ef27b475164dc2f898ede15114968e1af757a8a
frankie <frank@paycove.io>
2017-12-25 19:32:28 -0500

    added thread subscriptions with foreign key constraints and cascading delete method on the database migration level

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 TDD forum build with laravel and vue.js
 
 1. Thread
-2. Reply 
+2. Reply
 3. User
 
 A. Thread is created by a user
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -20,7 +20,7 @@ class ThreadTest extends TestCase
     {
         $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->thread->replies);
     }
-    
+
     /**
     * @test
     * @return void
@@ -54,5 +54,45 @@ class ThreadTest extends TestCase
     {
         $thread = create('App\Thread');
         $this->assertInstanceOf('App\Channel', $thread->channel);
+    }
+
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function a_thread_can_be_subscribed_to()
+    {
+        // Given we havea. thread
+
+        $thread = create('App\Thread');
+
+        // When the user subscribes to the thread
+
+        $thread->subscribe($userId = 1);
+
+        // Then we should be able to fetch all threads that the user subscribed to.
+
+        $this->assertEquals(1, $thread->subscriptions()->where('user_id', $userId)->count());
+    }
+
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function a_thread_can_be_unsubscribed_from()
+    {
+        // Given we havea. thread
+
+        $thread = create('App\Thread');
+
+        // When the user subscribes to the thread
+
+        $thread->unsubscribe($userId = 1);
+
+        // Then we should be able to fetch all threads that the user subscribed to.
+
+        $this->assertEquals(0, $thread->subscriptions()->where('user_id', $userId)->count());
     }
 }
